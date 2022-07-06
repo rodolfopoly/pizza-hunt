459fae45758262d6a441cf6735c38202bb2ea738
rodolfopoly <rodolfopoly@gmail.com>
2022-07-06 12:32:24 -0400

    user controllers finished

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -94,6 +94,40 @@ const userController = {
       res.status(400).json(err);
     };
   },
+  //add a new friend to a user's friend list
+  async addFriend({ params }, res) {
+    try{
+      const userData = await User.findOneAndUpdate(
+        { _id: params.userId },
+        { $push: { friends: params.friendId } },
+        {new: true}
+        )
+        if (!userData) {
+          res.status(404).json({ message: 'No notebook found with this id!' });
+        }
+        else{res.json(userData);}
+      } catch(err){
+          console.log(err);
+          res.status(400).json(err);
+      }
+  },
+  async deleteFriend({ params }, res) {
+    try{
+      const userData = await User.findOneAndUpdate(
+        { _id: params.userId },
+        { $pull: { friends: params.friendId } },
+        {new: true}
+        )
+        if (!userData) {
+          res.status(404).json({ message: 'No notebook found with this id!' });
+        }
+        else{res.json(userData);}
+      } catch(err){
+          console.log(err);
+          res.status(400).json(err);
+      }
+  }
+
 };
 
 module.exports = userController;
